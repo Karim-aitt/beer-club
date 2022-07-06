70e715496c147c7e2f6fb9a428ec0aa54f608673
arques89 <arquesjavier@gmail.com>
2022-07-06 19:40:08 +0000

    Acabado el POST del signup y beer

--- src/api/models.py
+++ src/api/models.py
@@ -1,12 +1,17 @@
 from flask_sqlalchemy import SQLAlchemy
+import datetime
 
 db = SQLAlchemy()
 
 class User(db.Model):
+    __tablename__="user"
     id = db.Column(db.Integer, primary_key=True)
+    nickname = db.Column(db.String, unique=True, nullable=False)
+    name = db.Column(db.String, nullable=False)
+    surnames = db.Column(db.String, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    password = db.Column(db.String(80),  nullable=False)
+    is_active = db.Column(db.Boolean(), nullable=False)
 
     def __repr__(self):
         return f'<User {self.email}>'
@@ -14,6 +19,119 @@ class User(db.Model):
     def serialize(self):
         return {
             "id": self.id,
+            "nickname": self.nickname,
+            "name": self.name,
+            "surnames": self.surnames,
             "email": self.email,
             # do not serialize the password, its a security breach
+        }
+
+class Category(db.Model):
+    __tablename__="category"
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, unique=True, nullable=False)
+    image = db.Column(db.String, nullable=False)
+    description = db.Column(db.String(255), nullable=False)
+
+    def __repr__(self):
+        return f'<Category {self.name}>'
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "image": self.image,
+            "description": self.description,
+        }
+class Beer(db.Model):
+    __tablename__="beer"
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    category_id = db.Column(db.Integer, db.ForeignKey('category.id'))
+    image = db.Column(db.String(120))
+    name = db.Column(db.String(80), unique=True)
+    smell = db.Column(db.String(80))
+    source = db.Column(db.String(80))
+    alcohol = db.Column(db.String(80))
+    company = db.Column(db.String(80))
+    description = db.Column(db.String(255))
+    creation_date = db.Column(db.DateTime, default=datetime.datetime.utcnow)
+    user = db.relationship('User')
+    category = db.relationship('Category')
+
+    def __repr__(self):
+        return f'<Beer {self.name}>'
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "user_id": self.user_id,
+            "category_id": self.category_id,
+            "image": self.image,
+            "name": self.name,
+            "smell": self.smell,
+            "source": self.source,
+            "alcohol": self.alcohol,
+            "company": self.company,
+            "description": self.description,
+            "creation_date": self.creation_date,
+        }
+class ILikeIt(db.Model):
+    __tablename__="ilikeit"
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    beer_id = db.Column(db.Integer, db.ForeignKey('beer.id'))
+    user = db.relationship('User')
+    beer = db.relationship('Beer')
+
+    def __repr__(self):
+        return f'<ILikeIt {self.id}>'
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "user_id": self.user_id,
+            "beer_id": self.beer_id,
+        }
+
+class Vote(db.Model):
+    __tablename__="vote"
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    beer_id = db.Column(db.Integer, db.ForeignKey('beer.id'))
+    punctuation = db.Column(db.Integer,)
+    user = db.relationship('User')
+    beer = db.relationship('Beer')
+
+    def __repr__(self):
+        return f'<Vote {self.punctuation}>'
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "user_id": self.user_id,
+            "beer_id": self.beer_id,
+            "punctuation": self.punctuation,
+        }
+
+class Comment(db.Model):
+    __tablename__="comment"
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    beer_id = db.Column(db.Integer, db.ForeignKey('beer.id'))
+    comment = db.Column(db.String, nullable=False)
+    creation_date = db.Column(db.Date, nullable=False)
+    user = db.relationship('User')
+    beer = db.relationship('Beer')
+
+    def __repr__(self):
+        return f'<Vote {self.comment}>'
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "user_id": self.user_id,
+            "beer_id": self.beer_id,
+            "comment": self.comment,
+            "creation_date": self.creation_date,
         }
--- src/api/routes.py
+++ src/api/routes.py
@@ -2,14 +2,71 @@
 This module takes care of starting the API Server, Loading the DB and Adding the endpoints
 """
 from flask import Flask, request, jsonify, url_for, Blueprint
-from api.models import db, User
+from api.models import db, User, Beer, Category, ILikeIt, Vote, Comment
 from api.utils import generate_sitemap, APIException
-
+import  bcrypt
 api = Blueprint('api', __name__)
+##----------------TABLE USER----------------##
+
+#____________CREATE USER____________#
+
+@api.route('/signup', methods=['POST'])
+def add_Signup():
+   
+    body = request.get_json()
+
+    user_check_email = User.query.filter_by(email=body['email']).first()
+    if user_check_email != None:
+        raise APIException('Ya existe este email')
+    
+    user_check_nickname = User.query.filter_by(nickname=body['nickname']).first()
+    if user_check_nickname != None:
+        raise APIException('Ya existe este nickname')
+
+    user = User(nickname=body["nickname"],name=body["name"],surnames=body["surnames"],email=body["email"],password=body["password"],is_active=True)
+    
+
+    db.session.add(user)
+    db.session.commit()
+
+    # data = {
+    # 'email': user.email,
+    # 'user_id': user.id
+    # }
+    # token = create_access_token(identity=data)
+    # db.session.commit()
+    # return jsonify(token)
+
+    return jsonify("ok"), 201
+
+##----------------TABLE BEER----------------##
+
+#____________CREATE BEER____________#
+
+@api.route('/beer', methods=['POST'])
+def add_Beer():
+   
+    body = request.get_json()
+
+    beer_check_name = Beer.query.filter_by(name=body['name']).first()
+    if beer_check_name != None:
+        raise APIException('Ya existe este nombre de cerveza')
+
+    beer = Beer(image=body["image"],name=body["name"],smell=body["smell"],source=body["source"],alcohol=body["alcohol"],company=body["company"],description=body["description"])
+
+    db.session.add(beer)
+    db.session.commit()
+
+    return jsonify("ok"), 201
 
 
-@api.route('/hello', methods=['POST', 'GET'])
-def handle_hello():
+
+
+
+#____________C____________#
+
+@api.route('/login', methods=['GET'])
+def Login():
 
     response_body = {
         "message": "Hello! I'm a message that came from the backend, check the network tab on the google inspector and you will see the GET request"

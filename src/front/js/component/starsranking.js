bef710d46c989f14d84d11d8c72f9aadd20e7794
Karim <karim.gonzalez.ait@gmail.com>
2022-07-13 20:04:46 +0000

    favorites completed

--- src/front/js/component/starsranking.js
+++ src/front/js/component/starsranking.js
@@ -1,62 +1,63 @@
-import React, {useState, useEffect, useContext} from "react";
+import React, { useState, useEffect, useContext } from "react";
 import { Context } from "../store/appContext";
-import "../../styles/starsranking.css"
-import config from "../config"
+import "../../styles/starsranking.css";
+import config from "../config";
 
 export const Stars = (props) => {
-    const {store, actions} = useContext(Context)
-    const [punctuation, setPunctuation] = useState(0);
-    const [hover, setHover] = useState(0);
-    const [beer_id, setBeer_id] = useState();
-    const id_beer = props.beer_id_star
-    // console.log(beer_id)
-    const token = localStorage.getItem('token')
+  const { store, actions } = useContext(Context);
+  const [punctuation, setPunctuation] = useState(0);
+  const [hover, setHover] = useState(0);
+  const [beer_id, setBeer_id] = useState();
+  const id_beer = props.beer_id_star;
+  // console.log(beer_id)
+  const token = localStorage.getItem("token");
 
-    
-    useEffect(() => {
-        if(punctuation == 0){
-          ""
-        } else {
-
-          fetch(`${config.hostname}/api/vote`, {
-              method: 'POST',
-              body: JSON.stringify({beer_id, punctuation}),
-              headers: {
-                  'Content-Type': 'application/json',
-                  'Authorization': "Bearer " + token
-              }
-          })
-          .then(res => {
-              if(res.status == 200){
-                  return "ok en el starsranking 31"
-              }
-          })
-          .then(data => {data} )// {data} solo funciona cuando el usuario ya ha votado a una misma cerveza. {data} retorna "True"
+  const onVote = (value) => {
+    console.log({ value });
+    fetch(`${config.hostname}/api/vote`, {
+      method: "POST",
+      body: JSON.stringify({
+        beer_id: props.beer_id_star,
+        punctuation: value,
+      }),
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: "Bearer " + token,
+      },
+    })
+      .then((res) => {
+        if (res.status == 200) {
+          return "ok en el starsranking 31";
         }
-    }, [punctuation])
-    
-    return (
-      <div className="star-rating">
-        {[...Array(5)].map((star, index) => {
-          index += 1;
-          return (
-            // Checked star icon is fa fa-star
-            // Without check star icon is far fa-star
-            <button
-              type="button"
-              key={index}
-              className={index <= (hover || punctuation) ? "fa fa-star" : "far fa-star"}
-              onClick={() => {
-                setPunctuation(index)
-                setBeer_id(id_beer)
-              }}
-              onMouseEnter={() => setHover(index)}
-              onMouseLeave={() => setHover(punctuation)}
-            >
-              <span className=""></span>
-            </button>
-          );
-        })}
-      </div>
-    );
+      })
+      .then((data) => {
+        data;
+      });
+  };
+
+  return (
+    <div className="star-rating">
+      {[...Array(5)].map((_, index) => {
+        index += 1;
+        return (
+          // Checked star icon is fa fa-star
+          // Without check star icon is far fa-star
+          <button
+            type="button"
+            key={index}
+            className={
+              index <= (hover || props.punctuation)
+                ? "fa fa-star"
+                : "far fa-star"
+            }
+            onClick={() => props.punctuation <= 0 && onVote(index)}
+            onMouseEnter={() => props.punctuation <= 0 && setHover(index)}
+            onMouseLeave={() => props.punctuation <= 0 && setHover(punctuation)}
+          >
+            <span className=""></span>
+          </button>
+        );
+      })}
+    </div>
+  );
 };
--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -1,33 +1,54 @@
 import React, { useContext, useEffect } from "react";
-import "../../js/config.js"
+import "../../js/config.js";
 import { Context } from "../store/appContext";
-import { Bodybar } from "../component/bodybar.js"
+import { Bodybar } from "../component/bodybar.js";
 import { Underseccion } from "../component/under-seccion.js";
 
-import { Card } from "../component/card"
+import { Card } from "../component/card";
 
 import "../../styles/home.css";
 
 export const Home = () => {
   const { store, actions } = useContext(Context);
-  
+
+  const getUserVote = (beer) => {
+    for (let index = 0; index < store.userDataVotes?.length; index++) {
+      const objVote = store.userDataVotes[index];
+      if (objVote?.beer_id === beer.id) {
+        return objVote.punctuation;
+      }
+    }
+
+    return 0;
+  };
+
   return (
     <div className="home-width mx-auto pb-3 bg-white">
       <Bodybar />
       <div className="container d-flex justify-content-center">
-        <Card rank="2" ribbon={false}/>
-        <Card rank="1" ribbon={false}/>
-        <Card rank="3" ribbon={false}/>
+        <Card rank="2" ribbon={false} />
+        <Card rank="1" ribbon={false} />
+        <Card rank="3" ribbon={false} />
       </div>
       <div className="d-flex mx-5 scrollmenu-x border mb-3">
-        {store.beers.length > 0 ? store.beers.map((elem, i) => {
-          return(
-            <Card key={i} nombre={elem.name} img={elem.image} ribbon={true} beer_id_card={elem.id}/>
-          )
-        }): <></>}
+        {store.beers.length > 0 ? (
+          store.beers.map((elem, i) => {
+            return (
+              <Card
+                key={i}
+                punctuation={getUserVote(elem)}
+                nombre={elem.name}
+                img={elem.image}
+                ribbon={true}
+                beer_id_card={elem.id}
+              />
+            );
+          })
+        ) : (
+          <></>
+        )}
       </div>
       <Underseccion />
-      
     </div>
   );
 };
